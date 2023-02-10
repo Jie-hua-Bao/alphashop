3e43490e4a086507a9c3cc42dc6f57079a9e686a
樺 <xd_301@yahoo.com.tw>
2023-02-10 16:16:22 +0800

    建立Header元件，尚未做細分，修改成JSX寫法以及引入SVG

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "./components/header/Header";
+import { ReactComponent as SvgSearchIcon } from "././icons/search.svg";
+
+
+// 引入CSS
+import "./style/css/reset.css";
+import "./style/css/base.css";
+import "./style/css/dark-theme.css";
+import "./style/css/main.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
