e86e5753b986eba16fdb81fde0f4ea6eed3f0e8d
樺 <xd_301@yahoo.com.tw>
2023-02-10 14:53:06 +0800

    資料夾建立，先拆分元件並建立檔案，svg檔 及 css 檔案引入

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as SvgSearchIcon } from "././icons/search.svg";
+import { ReactComponent as SvgToggleIcon } from "././icons/toggle.svg";
+import { ReactComponent as SvgCartIcon } from "././icons/cart.svg";
+import { ReactComponent as SvgMoonIcon } from "././icons/moon.svg";
+
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
