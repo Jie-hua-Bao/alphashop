8152c65fd950ebe5b1e7f3cfc89799a959d6ef5b
樺 <xd_301@yahoo.com.tw>
2023-02-12 14:25:46 +0800

    建立Product元件

--- src/components/main/Cart.jsx
+++ src/components/main/Cart.jsx
@@ -3,17 +3,50 @@ import { ReactComponent as SvgIconMinus } from "../../imagefiles/icons/minus.svg
 import { ReactComponent as SvgPlus } from "../../imagefiles/icons/plus.svg";
 import Product1 from "../../imagefiles/images/product-1.jpg";
 import Product2 from "../../imagefiles/images/product-2.jpg";
+function Product({ count, price, img, title }) {
+  return (
+    <div
+      className="product-container col col-12"
+      data-count={count}
+      data-price={price}
+    >
+      <img className="img-container" src={img} alt="" />
+      <div className="product-info">
+        <div className="product-name">{title}</div>
+        <div className="product-control-container">
+          <div className="product-control">
+            <svg className="product-action minus">
+              <SvgIconMinus />
+            </svg>
+            <span className="product-count">{count}</span>
+            <svg className="product-action plus">
+              <SvgPlus />
+            </svg>
+          </div>
+        </div>
+        <div className="price">{price}</div>
+      </div>
+    </div>
+  );
+}
+
 function Cart() {
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
       <section className="product-list col col-12" data-total-price="0">
-        <div
+        <Product
+          count={0}
+          price={3999}
+          title="破壞補丁修身牛仔褲"
+          img={Product1}
+        />
+        {/* <div
           className="product-container col col-12"
           data-count="0"
           data-price="3999"
         >
-          <img className="img-container" src={Product1} />
+          <img className="img-container" src={Product1} alt="" />
           <div className="product-info">
             <div className="product-name">破壞補丁修身牛仔褲</div>
             <div className="product-control-container">
@@ -29,13 +62,14 @@ function Cart() {
             </div>
             <div className="price">399</div>
           </div>
-        </div>
-        <div
+        </div> */}
+        <Product count={0} price={2999} title="刷色直筒牛仔褲" img={Product2} />
+        {/* <div
           className="product-container col col-12"
           data-count="0"
           data-price="1299"
         >
-          <img className="img-container" src={Product2} />
+          <img className="img-container" src={Product2} alt="" />
           <div className="product-info">
             <div className="product-name">刷色直筒牛仔褲</div>
             <div className="product-control-container">
@@ -51,7 +85,7 @@ function Cart() {
             </div>
             <div className="price">299</div>
           </div>
-        </div>
+        </div> */}
       </section>
       <section className="cart-info shipping col col-12">
         <div className="text">運費</div>
--- src/components/main/Public.jsx
+++ src/components/main/Public.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+// import React from "react";
 
-
-
-export function Input({ id, type, name, placeholder }) {
-  return <input id={id} type={type} name={name} placeholder={placeholder} />;
-}
-export function Label({ forinputid, labelname = "" }) {
-  return <label for={forinputid} className={labelname}></label>;
-}
-export function Select({ value, option }) {
-  return (
-    <select>
-      <option value={value}>{option}</option>
-    </select>
-  );
-}
+// export function Input({ id, type, name, placeholder }) {
+//   return <input id={id} type={type} name={name} placeholder={placeholder} />;
+// }
+// export function Label({ forinputid, labelname = "" }) {
+//   return <label for={forinputid} className={labelname}></label>;
+// }
+// export function Select({ value, option }) {
+//   return (
+//     <select>
+//       <option value={value}>{option}</option>
+//     </select>
+//   );
+// }
