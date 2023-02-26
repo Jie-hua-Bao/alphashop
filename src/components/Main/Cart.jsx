6de9596891a955cad1dc6cd437fb78f668962caa
樺 <xd_301@yahoo.com.tw>
2023-02-27 02:16:08 +0800

    購物車 +- 計算金額

--- src/components/Main/Cart.jsx
+++ src/components/Main/Cart.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as SvgIconMinus } from "../../imagefiles/icons/minus.svg";
 import { ReactComponent as SvgPlus } from "../../imagefiles/icons/plus.svg";
-import Product1 from "../../imagefiles/images/product-1.jpg";
-import Product2 from "../../imagefiles/images/product-2.jpg";
+// import Product1 from "../../imagefiles/images/product-1.jpg";
+// import Product2 from "../../imagefiles/images/product-2.jpg";
 import cardData from "../../data/cardData";
 
-function Product({ quantity, price, img, name }) {
+function Product({ id, setTotle, price, img, name }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function handlePlus() {
+    setQuantity(quantity + 1);
+    setTotle((total) => total + price);
+  }
+  function handleMinus() {
+    if (quantity === 0) return;
+    setQuantity(quantity - 1);
+    setTotle((total) => total - price);
+  }
   return (
     <div
       className="product-container col col-12"
       data-count={quantity}
       data-price={price}
+      key={id}
+      id={id}
     >
       <img className="img-container" src={img} alt="" />
       <div className="product-info">
         <div className="product-name">{name}</div>
         <div className="product-control-container">
           <div className="product-control">
-            <SvgIconMinus className="product-action minus" />
+            <SvgIconMinus
+              className="product-action minus"
+              onClick={handleMinus}
+            />
             <span className="product-count">{quantity}</span>
-            <SvgPlus className="product-action plus" />
+            <SvgPlus className="product-action plus" onClick={handlePlus} />
           </div>
         </div>
-        <div className="price">${price}</div>
+        <div className="price">{price}</div>
       </div>
     </div>
   );
@@ -38,16 +54,17 @@ function Price({ price, title }) {
 }
 
 function Cart() {
+  const [total, setTotle] = useState(0);
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
       <section className="product-list col col-12" data-total-price="0">
         {cardData.map((card) => (
-          <Product {...card} key={card.id} />
+          <Product {...card} setTotle={setTotle} />
         ))}
       </section>
       <Price title="運費" price={"免費"} />
-      <Price title="小計" price={"$" + 0} />
+      <Price title="小計" price={total} />
     </section>
   );
 }
